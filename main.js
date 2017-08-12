4f2acf2a32abbd3787c3ded6d255f5a8447f9beb
Leon Salsiccia <leon.salsiccia@gmail.com>
2017-08-12 23:13:21 +1000

    fixed js for new scss

--- main.js
+++ main.js
@@ -1,54 +1,61 @@
 var colourList = ['#ff8787', '#f783ac', '#da77f2', '#748ffc', '#3bc9db', '#69db7c', '#ffa94d'];
 
-var x = "hi";
 
-
+function defineVariables(programNo) {
+    textInput = document.getElementById(programNo + "textInput");
+    textOutput = document.getElementById(programNo + "textOutput");
+    content = document.getElementById(programNo + "content");
+    header = document.getElementById(programNo + 'header')
+}
 
 function firstRun() {
     var randomNumber = Math.floor(Math.random() * colourList.length);
-    var first_textInput = document.querySelectorAll('.firstProgram .textInput');
-    var first_textOutput = document.getElementById("textOutput");
-    console.log(first_textOutput);
-    console.log(first_textInput.children);
-    //var first_body = document.getElementById("first_body");
-    //var first_header = document.getElementById('first_header')
+    defineVariables("first_");
 
-    //first_textOutput.style.transition = "all 0.6s, opacity 0.3s 0.5s";
-    //first_body.style.transition = "all 0.4s";
+    textOutput.style.transition = "all 0.6s, opacity 0.3s 0.5s";
+    content.style.transition = "all 0.4s";
 
-    first_textOutput.innerHTML = 'You are so ' + first_textInput.value + x + '!';
-    first_textOutput.style.background = colourList[randomNumber];
-    first_textOutput.style.letterSpacing = randomNumber + 'px';
-    //first_body.style.height = "310px";
-    //first_textOutput.style.opacity = "1";
+    textOutput.innerHTML = 'You are so ' + textInput.value + '!';
+    textOutput.style.background = colourList[randomNumber];
+    textOutput.style.letterSpacing = randomNumber + 'px';
+    content.style.height = "310px";
+    textOutput.style.opacity = "1";
 
-    //if (first_textInput.value === 'Leon') {
-    //    first_textInput.style.border = "solid 2px rgb(58, 222, 133)";
+    if (textInput.value === 'Leon') {
+        textInput.style.border = "solid 2px rgb(58, 222, 133)";
     }
-//}
-/*
+}
+
 function hideTextOutput() {
-    first_textOutput.style.transition = "opacity 0.3s 0s";
-    first_textOutput.style.opacity = "0"
-    first_body.style.transition = "all 0.5s, height 0.4s 0.5s, padding 0.4s 0.5s";
-    first_body.style.height = "255px";
+    textOutput.style.transition = "opacity 0.3s 0s";
+    textOutput.style.opacity = "0"
+    content.style.transition = "all 0.5s, height 0.4s 0.5s, padding 0.4s 0.5s";
+    content.style.height = "255px";
 }
 
-function minimise() {
-    hideTextOutput();
-    first_textInput.value = ""
-    first_header.style.borderRadius = "18px";
-    first_body.style.transition= "all 0.4s, height 0.4s 0.7s, padding 0.4s 0.7s";
-    first_body.style.opacity = "0";
-    first_body.style.height = "0";
-    first_body.style.padding = "0";
+function minimise(programNo) {
+
+    defineVariables(programNo)
+
+    if (programNo === "first_") {
+        hideTextOutput();
+    }
+
+    textInput.value = ""
+    header.style.borderRadius = "18px";
+    content.style.transition= "all 0.4s, height 0.4s 0.7s, padding 0.4s 0.7s";
+    content.style.opacity = "0";
+    content.style.height = "0";
+    content.style.padding = "0";
 }
 
-function maximise() {
-    first_header.style.borderRadius = "18px 18px 0 0";
-    first_body.style.transition= "all 0.4s 0.7s, height 0.4s 0s, padding 0.4s 0s";
-    first_body.style.opacity = "1";
-    first_body.style.height = "255px";
-    first_body.style.padding = "26px 0";
+function maximise(programNo) {
+
+    defineVariables(programNo)
+    
+    header.style.borderRadius = "18px 18px 0 0";
+    content.style.transition= "all 0.4s 0.7s, height 0.4s 0s, padding 0.4s 0s";
+    content.style.opacity = "1";
+    content.style.height = "255px";
+    content.style.padding = "26px 0";
 }
-*/
